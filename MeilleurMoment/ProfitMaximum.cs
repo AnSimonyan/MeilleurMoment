ba05f60a2f5c15d526b648296768da7c8c550bb0
AnSimonyan <anna.simona1976@gmail.com>
2024-06-14 23:39:44 +0300

    Final version

---
+++ MeilleurMoment/ProfitMaximum.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeilleurMoment
+{
+    internal class ProfitMaximum
+
+    {
+        public int ProfitMaximumForPriceArray(int[] priceArrayByDays) 
+        {
+            int startIbdex = 0; int arrayLength = priceArrayByDays.Length;
+            List<int> profitlist = new List<int>();
+
+            while (startIbdex < arrayLength-1)
+            {
+                int endIndex = GetMaxProfitIndex (startIbdex, priceArrayByDays);
+
+                int profitToIndex = СalculateProfitByIndexes(startIbdex, endIndex, priceArrayByDays);
+
+                AddToProfitList(profitlist, profitToIndex);
+
+                startIbdex= endIndex+1;
+            }
+            return profitlist.Sum();
+        }
+
+        public int GetMaxProfitIndex(int startIndex,int[] priceArrayByDays) 
+        {
+            int arrayLength     = priceArrayByDays.Length;
+            int maxProfitIndex  = startIndex;
+
+            for (int i = startIndex; i < arrayLength-1; i++) 
+            {
+                if (priceArrayByDays[maxProfitIndex] < priceArrayByDays[i + 1])
+                {
+                    maxProfitIndex = i+1;
+                }
+                else 
+                {
+                    break;
+                } 
+            }
+            return maxProfitIndex;
+        }
+
+        public int СalculateProfitByIndexes(int startIndex,int endIndex, int[] priceArrayByDays)
+        { 
+            int profitResultat = 0;
+
+            if (startIndex >= 0 && endIndex < priceArrayByDays.Length)
+            {
+                profitResultat = priceArrayByDays[endIndex] - priceArrayByDays[startIndex];
+            }
+            return profitResultat;
+        }
+
+        private void AddToProfitList(List<int> profitList, int profit)
+        {
+            profitList.Add(profit);
+        }
+
+    }
+}
--- MeilleurMoment/Program.cs
+++ MeilleurMoment/Program.cs
@@ -4,9 +4,11 @@
     {
         static void Main(string[] args)
         {
-            int[] pricesByDays = new int[] { 7, 1, 4, 3, 2, 7 };
-            CalculProfits c = new CalculProfits();
-            int myProfit = c.CalculcalculateProfits(pricesByDays);
+            //int[] pricesByDays = new int[] { 1,2,3,4,5};
+            //int[] pricesByDays = new int[] { 7, 1, 5, 3, 6,4 };
+            int[] pricesByDays = new int[] { 7,6,4,3,1 };
+            ProfitMaximum c = new ProfitMaximum();
+            int myProfit = c.ProfitMaximumForPriceArray(pricesByDays);
             Console.WriteLine(myProfit);
         }
     }
